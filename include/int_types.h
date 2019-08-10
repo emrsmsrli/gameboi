844339909ee90b07a8b9368c2a1b0c1d24d41ebd
Emre <emrsmsrli1@gmail.com>
2019-08-10 18:22:59 +0300

    - configure gtest - add int_types.h

---
+++ include/int_types.h
@@ -0,0 +1,16 @@
+#ifndef GAMEBOY_INT_TYPES_H
+#define GAMEBOY_INT_TYPES_H
+
+#include <cstdint>
+
+using int8 = int8_t;
+using int16 = int16_t;
+using int32 = int32_t;
+using int64 = int64_t;
+
+using uint8 = uint8_t;
+using uint16 = uint16_t;
+using uint32 = uint32_t;
+using uint64 = uint64_t;
+
+#endif //GAMEBOY_INT_TYPES_H
