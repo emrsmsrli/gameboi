6cb390df333e5155a53c348f5bfccedc1077c7a3
Emre <emrsmsrli1@gmail.com>
2019-08-13 15:16:34 +0300

    add tests for half register

---
+++ test/TestHalfRegister.cpp
@@ -0,0 +1,79 @@
+
+#include <Register.h>
+#include "gtest/gtest.h"
+
+TEST(HalfRegister, Test) {
+    gameboy::Register r(0x0300);
+    ASSERT_TRUE(r.get_high().test(0));
+    ASSERT_TRUE(r.get_high().test(1));
+    r = 0x0100;
+    ASSERT_TRUE(r.get_high().test(0));
+    ASSERT_FALSE(r.get_high().test(1));
+}
+
+TEST(HalfRegister, Set) {
+    gameboy::Register r(0x0000);
+    r.get_high().set(4);
+    ASSERT_EQ(r.get_high(), 0x10);
+    r.get_low().set(0);
+    ASSERT_EQ(r.get_low(), 0x01);
+}
+
+TEST(HalfRegister, Reset) {
+    gameboy::Register r(0x0300);
+    r.get_high().reset(0);
+    ASSERT_EQ(r.get_high(), 0x02);
+}
+
+TEST(HalfRegister, Flip) {
+    gameboy::Register r(0x0000);
+    r.get_high().flip(0);
+    ASSERT_EQ(r.get_high(), 0x01);
+    r.get_high().flip(0);
+    ASSERT_EQ(r.get_high(), 0x00);
+}
+
+TEST(HalfRegister, All) {
+    gameboy::Register r(0x0000);
+    ASSERT_FALSE(r.get_high().all());
+    r = 0xFF00;
+    ASSERT_TRUE(r.get_high().all());
+}
+
+TEST(HalfRegister, Any) {
+    gameboy::Register r(0x0000);
+    ASSERT_FALSE(r.get_high().any());
+    r = 0xDE00;
+    ASSERT_TRUE(r.get_high().any());
+}
+
+TEST(HalfRegister, None) {
+    gameboy::Register r(0x0000);
+    ASSERT_TRUE(r.get_high().none());
+    r = 0xEC00;
+    ASSERT_FALSE(r.get_high().none());
+    ASSERT_TRUE(r.get_low().none());
+}
+
+TEST(HalfRegister, Assignment) {
+    gameboy::Register r(0xEECB);
+    r.get_low() = 0xDE;
+    ASSERT_EQ(r, 0xEEDE);
+}
+
+TEST(HalfRegister, IncDec) {
+    gameboy::Register r(0x0000);
+    ++r.get_low();
+    ++r.get_low();
+    ASSERT_EQ(r, 0x0002);
+    --r.get_low();
+    ASSERT_EQ(r, 0x0001);
+}
+
+TEST(HalfRegister, AddSub) {
+    gameboy::Register r(0x0001);
+    r.get_low() += 0x0D;
+    ASSERT_EQ(r, 0x000E);
+    r.get_low() -= 0x0C;
+    ASSERT_EQ(r, 0x0002);
+}
