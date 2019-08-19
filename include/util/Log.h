d8d0f724815e3dc906a0d5b806e946b65b58c23b
Emre <emrsmsrli1@gmail.com>
2019-08-19 21:06:39 +0300

    improve logging facilities

--- include/util/Log.h
+++ include/util/Log.h
@@ -7,17 +7,17 @@
 namespace gameboy::log {
     template<typename String, typename... Args>
     void info(const String& format, Args... args) {
-        std::cout << fmt::format("[I] - {}\n", format, args...);
+        std::cout << "[I] - " << fmt::format(format, args...) << '\n';
     }
 
     template<typename String, typename... Args>
     void warn(const String& format, Args... args) {
-        std::cout << fmt::format("[W] - {}\n", format, args...);
+        std::cout << "[W] - " << fmt::format(format, args...) << '\n';
     }
 
     template<typename String, typename... Args>
     void error(const String& format, Args... args) {
-        std::cout << fmt::format("[E] - {}\n", format, args...);
+        std::cerr << "[E] - " << fmt::format(format, args...) << '\n';
     }
 }
 
