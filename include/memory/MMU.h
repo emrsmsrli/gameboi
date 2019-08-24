602b8b8937166ef5f4371eeb0b9ace49c53afc0b
Emre <emrsmsrli1@gmail.com>
2019-08-24 14:28:21 +0300

    add first impl of memory management unit

---
+++ include/memory/MMU.h
@@ -0,0 +1,20 @@
+#ifndef GAMEBOY_MMU_H
+#define GAMEBOY_MMU_H
+
+#include <cstdint>
+#include <vector>
+
+namespace gameboy::memory {
+    class Address16;
+
+    class MMU {
+    public:
+        void write(const Address16& address, uint8_t data);
+        uint8_t read(const Address16& address);
+
+    private:
+        std::vector<uint8_t> memory;
+    };
+}
+
+#endif //GAMEBOY_MMU_H
---
+++ src/memory/MMU.cpp
@@ -0,0 +1,12 @@
+
+#include "memory/MMU.h"
+
+void gameboy::memory::MMU::write(const gameboy::memory::Address16& address, uint8_t data)
+{
+
+}
+
+uint8_t gameboy::memory::MMU::read(const gameboy::memory::Address16& address)
+{
+    return 0;
+}
