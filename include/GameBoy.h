6cc990aa78ef559dd5f3c30588ccfbee761a27af
Emre <emrsmsrli1@gmail.com>
2019-08-24 17:10:43 +0300

    add firs impl of main gameboy class

---
+++ include/GameBoy.h
@@ -0,0 +1,22 @@
+#ifndef GAMEBOY_GAMEBOY_H
+#define GAMEBOY_GAMEBOY_H
+
+#include <string>
+#include <memory>
+#include "cpu/CPU.h"
+#include "memory/MMU.h"
+
+namespace gameboy {
+    class GameBoy {
+    public:
+        explicit GameBoy(std::string_view cartridge_path);
+
+        void start();
+
+    private:
+        std::unique_ptr<memory::MMU> memory;
+        std::unique_ptr<cpu::CPU> cpu;
+    };
+}
+
+#endif //GAMEBOY_GAMEBOY_H
---
+++ src/GameBoy.cpp
@@ -0,0 +1,18 @@
+
+#include "GameBoy.h"
+
+namespace {
+    constexpr auto max_cycles = 70224;
+    constexpr auto fps = 59.73f;
+    constexpr auto delay = 1000.f / fps;
+}
+
+gameboy::GameBoy::GameBoy(std::string_view cartridge_path)
+{
+
+}
+
+void gameboy::GameBoy::start()
+{
+
+}
