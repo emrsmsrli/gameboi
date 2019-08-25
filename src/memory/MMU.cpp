11753592f768beeae976c1f86bf1ae3622406ac4
Emre <emrsmsrli1@gmail.com>
2019-08-25 11:24:02 +0300

    make mmu::read const

--- src/memory/MMU.cpp
+++ src/memory/MMU.cpp
@@ -6,7 +6,7 @@ void gameboy::memory::MMU::write(const gameboy::memory::Address16& address, uint
 
 }
 
-uint8_t gameboy::memory::MMU::read(const gameboy::memory::Address16& address)
+uint8_t gameboy::memory::MMU::read(const gameboy::memory::Address16& address) const
 {
     return 0;
 }
