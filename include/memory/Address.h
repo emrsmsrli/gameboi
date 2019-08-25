703d1f6190ff53d0ef14b45f41ee9ecbcfe25f4c
Emre Şimşirli <emre.simsirli@masomo.com>
2019-08-25 18:11:49 +0300

    make address8 and 16 constexpr

--- include/memory/Address.h
+++ include/memory/Address.h
@@ -14,10 +14,10 @@ namespace gameboy::memory {
      */
     class Address8 {
     public:
-        Address8() = default;
-        explicit Address8(uint8_t default_value) : value(default_value) {}
+        constexpr Address8() = default;
+        constexpr explicit Address8(uint8_t default_value) : value(default_value) {}
 
-        [[nodiscard]] uint8_t get_value() const { return value; }
+        [[nodiscard]] constexpr uint8_t get_value() const { return value; }
 
     private:
         uint8_t value = 0x00;
@@ -28,10 +28,10 @@ namespace gameboy::memory {
      */
     class Address16 {
     public:
-        Address16() = default;
-        explicit Address16(uint16_t default_value) : value(default_value) {}
+        constexpr Address16() = default;
+        explicit constexpr Address16(uint16_t default_value) : value(default_value) {}
 
-        [[nodiscard]] uint16_t get_value() const { return value; }
+        [[nodiscard]] constexpr uint16_t get_value() const { return value; }
 
     private:
         uint8_t value = 0x0000;
