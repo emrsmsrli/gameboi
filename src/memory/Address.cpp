0419df6c1c55d5da841a0752f36776babc2df46c
Emre <emrsmsrli1@gmail.com>
2019-08-25 17:11:03 +0300

    add some memory map locations and a make_address function for them

--- src/memory/Address.cpp
+++ src/memory/Address.cpp
@@ -2,6 +2,11 @@
 #include "memory/Address.h"
 #include "cpu/Register16.h"
 
+gameboy::memory::Address16 gameboy::memory::make_address(MemoryMapLocation location)
+{
+    return make_address(static_cast<uint16_t>(location));
+}
+
 gameboy::memory::Address8 gameboy::memory::make_address(uint8_t address)
 {
     return Address8(address);
