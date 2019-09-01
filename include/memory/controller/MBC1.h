38e135524724fbad147811f3a627fdb2e6f8d2df
Emre Şimşirli <emre.simsirli@masomo.com>
2019-09-01 22:38:55 +0300

    implement mbc1 control function, remove read and write overrides

--- include/memory/controller/MBC1.h
+++ include/memory/controller/MBC1.h
@@ -8,39 +8,13 @@
 #include <cstdint>
 #include "MBC.h"
 
-/**
- * 16Mbit ROM/8KByte RAM
- * 4bit ROM/32KByte RAM
- *
- * the MBC1 defaults to 16Mbit ROM/8KByte RAM mode on power up.
- *
- * writing a value (XXXXXXXS - X = Don't care, S = Memory model select)
- * into 6000-7FFF area will select the memory model to use. S = 0 selects 16/8 mode. S = 1 selects 4/32 mode.
- *
- * writing a value (XXXBBBBB - X = Don't cares, B = bank select bits) into 2000-3FFF area will select
- * an appropriate ROM bank at 4000-7FFF. Values of 0    and 1 do the same thing and point to ROM bank 1
- *
- * rom bank 0 is not accessible from 4000-7FFF and can    only be read from 0000-3FFF
- *
- * memory model is set to 4/32:
- *     Writing a value (XXXXXXBB - X = Don't care, B = bank select bits) into 4000-5FFF area will
- *     select an appropriate RAM bank at A000-C000. Before you can read or write to a RAM bank you have to enable
- *     it by writing a XXXX1010 into 0000-1FFF area*. To disable RAM bank operations write any value but XXXX1010 into
- *     0000-1FFF area. Disabling a RAM bank probably protects that bank from false writes
- *     during power down of the GameBoy. (NOTE: Nintendo suggests values $0A to enable and $00 to disable RAM bank!!)
- * memory model is set to 16/8 mode:
- *     Writing a value (XXXXXXBB - X = Don't care, B = bank select bits) into 4000-5FFF area will set the
- *     two most significant ROM address lines.    * NOTE: The Super Smart Card doesn't require this
- *     operation because it's RAM bank is ALWAYS enabled.    Include this operation anyway to allow your code
- *     to work with both
- */
 namespace gameboy::memory::controller {
     class MBC1 : public MBC {
     public:
         MBC1(const std::vector<uint8_t>& rom, const CartridgeInfo& rom_header);
 
-        [[nodiscard]] uint8_t read(const Address16& address) const override;
-        void write(const Address16& address, uint8_t data) override;
+    protected:
+        void control(const Address16& virtual_address, uint8_t data) override;
 
     private:
         enum class Mode {
@@ -49,6 +23,9 @@ namespace gameboy::memory::controller {
         };
 
         Mode mode{Mode::rom_banking};
+
+        void correct_rom_bank();
+        void select_memory_mode(uint8_t data);
     };
 }
 
--- src/memory/controller/MBC1.cpp
+++ src/memory/controller/MBC1.cpp
@@ -9,23 +9,55 @@
 gameboy::memory::controller::MBC1::MBC1(const std::vector<uint8_t>& rom, const gameboy::CartridgeInfo& rom_header)
         : MBC(rom, rom_header) { }
 
-uint8_t gameboy::memory::controller::MBC1::read(const gameboy::memory::Address16& address) const
+void gameboy::memory::controller::MBC1::control(const gameboy::memory::Address16& virtual_address, uint8_t data)
 {
-    constexpr AddressRange external_ram_range(Map::ram_external_start, Map::ram_external_end);
-    if(external_ram_range.contains(address) && !is_external_ram_enabled) {
-        return 0xFFu;
-    }
+    constexpr AddressRange external_ram_enable_range(0x0000u, 0x1FFFu);
+    constexpr AddressRange rom_bank_select_range(0x2000u, 0x3FFFu);
+    constexpr AddressRange ram_bank_select_range(0x4000u, 0x5FFFu);
+    constexpr AddressRange memory_mode_select_range(0x6000u, 0x7FFFu);
 
-    return MBC::read(address);
+    if(external_ram_enable_range.contains(virtual_address)) {
+        is_external_ram_enabled = (data & 0x0Fu) == 0x0Au;
+    } else if(rom_bank_select_range.contains(virtual_address)) {
+        if(mode == Mode::rom_banking) {
+            selected_rom_bank = (selected_rom_bank & 0xE0u) | (data & 0x1Fu);
+        } else {
+            selected_rom_bank = data & 0x1Fu;
+        }
+
+        correct_rom_bank();
+    } else if(ram_bank_select_range.contains(virtual_address)) {
+        if(mode == Mode::rom_banking) {
+            selected_rom_bank = ((data & 0x03u) << 0x5u) | (selected_rom_bank & 0x1Fu);
+            correct_rom_bank();
+        } else {
+            selected_external_ram_bank = data & 0x03u;
+        }
+    } else if(memory_mode_select_range.contains(virtual_address)) {
+        select_memory_mode(data);
+    }
 }
 
-void gameboy::memory::controller::MBC1::write(const gameboy::memory::Address16& address, uint8_t data)
+void gameboy::memory::controller::MBC1::correct_rom_bank()
 {
-    if(AddressRange(0x0000, 0x1FFF).contains(address)) {
-        is_external_ram_enabled = (data & 0x0Fu) == 0x0A;
-    } else if(AddressRange(0x2000, 0x3FFF).contains(address)) {
-        // todo set selected_rom_bank
+    switch(selected_rom_bank) {
+        case 0x00:
+        case 0x20:
+        case 0x40:
+        case 0x60:
+            ++selected_rom_bank;
+        default:
+            break;
     }
 
-    MBC::write(address, data);
+    --selected_rom_bank;
+}
+
+void gameboy::memory::controller::MBC1::select_memory_mode(uint8_t data)
+{
+    if((data & 0x1u) == 0x1u) {
+        mode = Mode::ram_banking;
+    } else {
+        mode = Mode::rom_banking;
+    }
 }
