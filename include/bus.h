1db9e161f99e34a024e85ab7703a4d8b3cf792dd
Emre Şimşirli <emre.simsirli@masomo.com>
2019-10-12 19:49:21 +0300

    bus now contains all possible components of the gameboy

--- include/bus.h
+++ include/bus.h
@@ -2,19 +2,25 @@
 #define GAMEBOY_BUS_H
 
 #include <util/observer.h>
-#include <cpu/cpu.h>
-#include <ppu/ppu.h>
 
 namespace gameboy {
-    class bus {
-    public:
-        bus() = default;
 
-    private:
-        observer<cpu> cpu_;
-        observer<ppu> ppu_;
-        observer<cpu> apu_;
-    };
+class cartridge;
+class cpu;
+class ppu;
+class apu;
+
+class bus {
+public:
+    explicit bus(observer<cartridge> cartridge)
+        : cartridge(cartridge) {}
+
+    observer<cartridge> cartridge;
+    observer<cpu> cpu;
+    observer<ppu> ppu;
+    observer<apu> apu;
+};
+
 }
 
 #endif //GAMEBOY_BUS_H
