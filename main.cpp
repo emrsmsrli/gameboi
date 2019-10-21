ee6500926981c2aab09b0da50221cdec708cb078
Emre Şimşirli <emre.simsirli@masomo.com>
2019-10-21 21:44:30 +0300

    add debug code to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+
 #include <gameboy.h>
 
 int main(int /*argc*/, char** /*argv*/)
 {
     gameboy::gameboy gb("rom/path.rom");
     gb.start();
+
+    sf::RenderWindow window(
+        sf::VideoMode(160 * 4, 144 * 4),
+        "GAMEBOY",
+        sf::Style::Close | sf::Style::Titlebar);
+
+    while(window.isOpen()) {
+        sf::Event event{};
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed) {
+                window.close();
+            } else if(event.type == sf::Event::KeyReleased) {
+                if(event.key.code == sf::Keyboard::F) {
+                    gb.tick();
+                }
+            }
+        }
+    }
 
     return 0;
 }
