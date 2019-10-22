4f1d565f0cdbdf1bbd8f73b675116fec75ef65ab
Emre Şimşirli <emre.simsirli@masomo.com>
2019-10-22 21:40:56 +0300

    add enum class bit flag helper

---
+++ include/util/enumutil.h
@@ -0,0 +1,84 @@
+#ifndef GAMEBOY_ENUMUTIL_H
+#define GAMEBOY_ENUMUTIL_H
+
+#include <type_traits>
+
+namespace gameboy {
+
+namespace detail {
+
+template<typename>
+struct enable_bitmask_operators {
+    static constexpr bool value = false;
+};
+
+template<typename E>
+inline constexpr bool enable_bitmask_operators_v = enable_bitmask_operators<E>::value;
+
+}
+
+template<typename E>
+std::enable_if_t<detail::enable_bitmask_operators_v<E>, E>
+operator&(E lhs, E rhs)
+{
+    using underlying_type = std::underlying_type_t<E>;
+    return static_cast<E>(static_cast<underlying_type>(lhs) & static_cast<underlying_type>(rhs));
+}
+
+template<typename E>
+std::enable_if_t<detail::enable_bitmask_operators_v<E>, E>
+operator|(E lhs, E rhs)
+{
+    using underlying_type = std::underlying_type_t<E>;
+    return static_cast<E>(static_cast<underlying_type>(lhs) | static_cast<underlying_type>(rhs));
+}
+
+template<typename E>
+std::enable_if_t<detail::enable_bitmask_operators_v<E>, E>
+operator^(E lhs, E rhs)
+{
+    using underlying_type = std::underlying_type_t<E>;
+    return static_cast<E>(static_cast<underlying_type>(lhs) ^ static_cast<underlying_type>(rhs));
+}
+
+template<typename E>
+std::enable_if_t<detail::enable_bitmask_operators_v<E>, E>
+operator~(E e)
+{
+    using underlying_type = std::underlying_type_t<E>;
+    return static_cast<E>(~static_cast<underlying_type>(e));
+}
+
+template<typename E>
+std::enable_if_t<detail::enable_bitmask_operators_v<E>, E&>
+operator&=(E& lhs, E rhs)
+{
+    lhs = lhs & rhs;
+    return lhs;
+}
+
+template<typename E>
+std::enable_if_t<detail::enable_bitmask_operators_v<E>, E&>
+operator|=(E& lhs, E rhs)
+{
+    lhs = lhs | rhs;
+    return lhs;
+}
+
+template<typename E>
+std::enable_if_t<detail::enable_bitmask_operators_v<E>, E&>
+operator^=(E& lhs, E rhs)
+{
+    lhs = lhs ^ rhs;
+    return lhs;
+}
+
+}
+
+#define DEFINE_ENUM_CLASS_FLAGS(E) \
+template<> \
+struct gameboy::detail::enable_bitmask_operators<E> { \
+    static constexpr bool value = true; \
+} \
+
+#endif //GAMEBOY_ENUMUTIL_H
