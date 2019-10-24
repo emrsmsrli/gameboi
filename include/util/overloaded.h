a76db2816ae00e16ef183d2c6fe1c9acdeb0c636
Emre Şimşirli <emre.simsirli@masomo.com>
2019-10-24 20:46:42 +0300

    put overloaded into gameboy ns

--- include/util/overloaded.h
+++ include/util/overloaded.h
@@ -1,7 +1,11 @@
 #ifndef GAMEBOY_OVERLOADED_H
 #define GAMEBOY_OVERLOADED_H
 
+namespace gameboy {
+
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+} // namespace gameboy
 
 #endif //GAMEBOY_OVERLOADED_H
