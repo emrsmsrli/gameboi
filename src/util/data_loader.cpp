5c5dc63f6c38cc239ba6cb1cd126fb968e19aaf2
Emre Şimşirli <emrsmsrli1@gmail.com>
2019-11-02 14:59:54 +0300

    fix data loader would reserve but does not use an area of memory

--- src/util/data_loader.cpp
+++ src/util/data_loader.cpp
@@ -6,30 +6,19 @@
 
 namespace gameboy {
 
-auto get_file_size(std::ifstream& rom_file)
-{
-    const auto rom_size = rom_file.tellg();
-    rom_file.seekg(0, std::ios::beg);
-    return rom_size;
-}
-
 std::vector<uint8_t> data_loader::load(const std::string_view path)
 {
-    std::ifstream file(path.data(), std::ios::binary | std::ios::ate);
+    std::ifstream file(path.data(), std::ios::binary);
     file.unsetf(std::ios::skipws);
 
     if(!file.is_open()) {
         log::error("file could not be opened: {}", path);
     }
 
-    const auto rom_size = get_file_size(file);
-
-    std::vector<uint8_t> bytes(rom_size);
-
-    // read the data
-    bytes.insert(bytes.begin(),
-        std::istream_iterator<uint8_t>(file),
-        std::istream_iterator<uint8_t>());
+    std::vector<uint8_t> bytes{
+        std::istream_iterator<uint8_t>{file},
+        std::istream_iterator<uint8_t>{}
+    };
 
     return bytes;
 }
