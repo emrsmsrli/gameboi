1e84a96736f03b8407f2db4c291654a227e5fba6
Emre Şimşirli <emre.simsirli@masomo.com>
2019-11-06 00:01:38 +0300

    use mathutil in dma_transfer_data

--- include/ppu/dma_transfer_data.h
+++ include/ppu/dma_transfer_data.h
@@ -15,11 +15,11 @@ struct dma_transfer_data {
 
     uint16_t remaining_length = 0u;
 
-    [[nodiscard]] bool active() const noexcept { return math::bit_test(length_mode_start.value(), 7u); }
-    [[nodiscard]] uint16_t length() const noexcept { return ((length_mode_start & 0x7Fu) + 1) * 0x10; }
+    [[nodiscard]] bool active() const noexcept { return bit_test(length_mode_start, 7u); }
+    [[nodiscard]] uint16_t length() const noexcept { return (mask(length_mode_start, 0x7Fu) + 1) * 0x10; }
 
     // possible bug here. active flag might be 0 instead of 1 when dma is actually active.
-    void disable() noexcept { length_mode_start &= 0x7Fu; }
+    void disable() noexcept { length_mode_start = mask_set(length_mode_start, 0x7Fu); }
 };
 
 } // namespace gameboy
