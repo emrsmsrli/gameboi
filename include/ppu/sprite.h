9c7b3cb5bf015542dc77fcc0a8e52ce1134bff55
Emre Şimşirli <emre.simsirli@masomo.com>
2019-11-06 00:01:51 +0300

    use mathutil in sprite

--- include/ppu/sprite.h
+++ include/ppu/sprite.h
@@ -13,11 +13,11 @@ struct sprite {
     uint8_t character_code;
     uint8_t attributes;
 
-    [[nodiscard]] bool prioritized() const noexcept { return !math::bit_test(attributes, 7u); } // todo?
-    [[nodiscard]] bool v_flipped() const noexcept { return math::bit_test(attributes, 6u); }
-    [[nodiscard]] bool h_flipped() const noexcept { return math::bit_test(attributes, 5u); }
-    [[nodiscard]] uint8_t vram_bank() const noexcept { return math::bit_test(attributes, 3u) ? 0x1u : 0x0u; } // todo cgb only
-    [[nodiscard]] uint8_t palette_number() const noexcept { return attributes & 0x7u; } // todo cgb only
+    [[nodiscard]] bool prioritized() const noexcept { return !bit_test(attributes, 7u); } // todo?
+    [[nodiscard]] bool v_flipped() const noexcept { return bit_test(attributes, 6u); }
+    [[nodiscard]] bool h_flipped() const noexcept { return bit_test(attributes, 5u); }
+    [[nodiscard]] uint8_t vram_bank() const noexcept { return bit_test(attributes, 3u) ? 0x1u : 0x0u; } // todo cgb only
+    [[nodiscard]] uint8_t palette_number() const noexcept { return mask(attributes, 0x7u); } // todo cgb only
 
     [[nodiscard]] bool operator<(const sprite& other) const noexcept
     {
