723490d1e855cb3c7475c2ea292667cccd6684e3
Emre Şimşirli <emre.simsirli@masomo.com>
2019-11-06 00:00:05 +0300

    extend mathutils

--- include/util/mathutil.h
+++ include/util/mathutil.h
@@ -3,7 +3,7 @@
 
 #include <cstdint>
 
-namespace gameboy::math {
+namespace gameboy {
 
 template<typename T>
 bool bit_test(const T n, const uint32_t bit) noexcept
@@ -12,15 +12,74 @@ bool bit_test(const T n, const uint32_t bit) noexcept
 }
 
 template<typename T>
-void bit_set(T& n, const uint32_t bit) noexcept
+T bit_set(const T n, const uint32_t bit) noexcept
 {
-    n |= 0x1u << bit;
+    return n | (0x1u << bit);
 }
 
 template<typename T>
-void bit_reset(T& n, const uint32_t bit) noexcept
+T bit_reset(const T n, const uint32_t bit) noexcept
 {
-    n &= ~(0x1u << bit);
+    return n & ~(0x1u << bit);
+}
+
+template<typename T>
+T mask(const T n, const uint32_t m) noexcept
+{
+    return n & m;
+}
+
+template<typename T>
+bool mask_test(const T n, const uint32_t m) noexcept
+{
+    return (n & m) == m;
+}
+
+template<typename T>
+T mask_reset(const T n, const uint32_t m) noexcept
+{
+    return n & ~m;
+}
+
+template<typename T>
+T mask_set(const T n, const uint32_t m) noexcept
+{
+    return n | m;
+}
+
+inline bool half_carry(const uint8_t x, const uint8_t y) noexcept
+{
+    return (((x & 0x0Fu) + (y & 0x0Fu)) & 0x10u) != 0;
+}
+
+inline bool half_carry(const uint16_t x, const uint16_t y) noexcept
+{
+    return (((x & 0x0FFFu) + (y & 0x0FFFu)) & 0x1000u) != 0;
+}
+
+inline bool full_carry(const uint8_t x, const uint8_t y) noexcept
+{
+    return (((x & 0xFFu) + (y & 0xFFu)) & 0x100u) != 0;
+}
+
+inline bool full_carry(const uint16_t x, const uint16_t y) noexcept
+{
+    return (((x & 0xFFFFu) + ((y & 0xFFFFu))) & 0x10000u) != 0;
+}
+
+inline bool half_borrow(const uint8_t x, const uint8_t y) noexcept
+{
+    return (x & 0x0Fu) < (y & 0x0Fu);
+}
+
+inline bool full_borrow(const uint8_t x, const uint8_t y) noexcept
+{
+    return (x & 0xFFu) < (y & 0xFFu);
+}
+
+inline uint16_t word(const uint8_t high, const uint8_t low) noexcept
+{
+    return static_cast<uint16_t>(high) << 8u | low;
 }
 
 } // namespace gameboy
