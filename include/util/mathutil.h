f7f4bca216d247e933e4083534bdf5c3b8ab9777
Emre <emrsmsrli1@gmail.com>
2019-11-08 00:00:41 +0300

    make all math functions constexpr
    Signed-off-by: Emre <emrsmsrli1@gmail.com>

--- include/util/mathutil.h
+++ include/util/mathutil.h
@@ -6,73 +6,73 @@
 namespace gameboy {
 
 template<typename T>
-bool bit_test(const T n, const uint32_t bit) noexcept
+constexpr bool bit_test(const T n, const uint32_t bit) noexcept
 {
     return ((n >> bit) & 0x1u) == 0x1u;
 }
 
 template<typename T>
-T bit_set(const T n, const uint32_t bit) noexcept
+constexpr T bit_set(const T n, const uint32_t bit) noexcept
 {
     return n | (0x1u << bit);
 }
 
 template<typename T>
-T bit_reset(const T n, const uint32_t bit) noexcept
+constexpr T bit_reset(const T n, const uint32_t bit) noexcept
 {
     return n & ~(0x1u << bit);
 }
 
 template<typename T>
-T mask(const T n, const uint32_t m) noexcept
+constexpr T mask(const T n, const uint32_t m) noexcept
 {
     return n & m;
 }
 
 template<typename T>
-bool mask_test(const T n, const uint32_t m) noexcept
+constexpr bool mask_test(const T n, const uint32_t m) noexcept
 {
     return (n & m) == m;
 }
 
 template<typename T>
-T mask_reset(const T n, const uint32_t m) noexcept
+constexpr T mask_reset(const T n, const uint32_t m) noexcept
 {
     return n & ~m;
 }
 
 template<typename T>
-T mask_set(const T n, const uint32_t m) noexcept
+constexpr T mask_set(const T n, const uint32_t m) noexcept
 {
     return n | m;
 }
 
-inline bool half_carry(const uint8_t x, const uint8_t y) noexcept
+constexpr bool half_carry(const uint8_t x, const uint8_t y) noexcept
 {
     return (((x & 0x0Fu) + (y & 0x0Fu)) & 0x10u) != 0;
 }
 
-inline bool half_carry(const uint16_t x, const uint16_t y) noexcept
+constexpr bool half_carry(const uint16_t x, const uint16_t y) noexcept
 {
     return (((x & 0x0FFFu) + (y & 0x0FFFu)) & 0x1000u) != 0;
 }
 
-inline bool full_carry(const uint8_t x, const uint8_t y) noexcept
+constexpr bool full_carry(const uint8_t x, const uint8_t y) noexcept
 {
     return (((x & 0xFFu) + (y & 0xFFu)) & 0x100u) != 0;
 }
 
-inline bool full_carry(const uint16_t x, const uint16_t y) noexcept
+constexpr bool full_carry(const uint16_t x, const uint16_t y) noexcept
 {
     return (((x & 0xFFFFu) + ((y & 0xFFFFu))) & 0x10000u) != 0;
 }
 
-inline bool half_borrow(const uint8_t x, const uint8_t y) noexcept
+constexpr bool half_borrow(const uint8_t x, const uint8_t y) noexcept
 {
     return (x & 0x0Fu) < (y & 0x0Fu);
 }
 
-inline bool full_borrow(const uint8_t x, const uint8_t y) noexcept
+constexpr bool full_borrow(const uint8_t x, const uint8_t y) noexcept
 {
     return (x & 0xFFu) < (y & 0xFFu);
 }
