a8275928e9d6265d979f8d0a167127ab91a8c9ac
Emre Şimşirli <emrsmsrli1@gmail.com>
2019-11-21 22:32:54 +0300

    add constructors for palette and color

--- include/ppu/data/palette.h
+++ include/ppu/data/palette.h
@@ -8,20 +8,21 @@
 namespace gameboy {
 
 struct palette {
-    std::array<color, 4> colors = {
-        color{255, 255, 255},
-        color{192, 192, 192},
-        color{96, 96, 96},
-        color{0, 0, 0},
-    };
+    std::array<color, 4> colors;
 
-    [[nodiscard]] constexpr palette from(uint8_t data) const noexcept
+    constexpr palette() = default;
+    constexpr explicit palette(const color& color)
+	: palette{color, color, color, color} {}
+    constexpr palette(const color& c_1, const color& c_2, const color& c_3, const color& c_4)
+	: colors{c_1, c_2, c_3, c_4} {}
+
+    [[nodiscard]] constexpr static palette from(const palette& reference, const uint8_t data) noexcept
     {
         palette p{};
 
         for(uint8_t i = 0; i < 4; ++i) {
             const auto index = (data & 0x3u << i) >> i;
-            p.colors[i] = colors[index];
+            p.colors[i] = reference.colors[index];
         }
 
         return p;
