08513a1cf36461e4f684b8ea74b462a8ca22027c
Emre <emrsmsrli1@gmail.com>
2019-12-02 10:56:52 +0300

    add fixme tag

--- include/ppu/dma_transfer_data.h
+++ include/ppu/dma_transfer_data.h
@@ -18,7 +18,7 @@ struct dma_transfer_data {
     [[nodiscard]] bool active() const noexcept { return bit_test(length_mode_start, 7u); }
     [[nodiscard]] uint16_t length() const noexcept { return ((length_mode_start & 0x7Fu) + 1) * 0x10; }
 
-    // possible bug here. active flag might be 0 instead of 1 when dma is actually active.
+    // fixme possible bug here. active flag might be 0 instead of 1 when dma is actually active.
     void disable() noexcept { length_mode_start = mask_set(length_mode_start, 0x7Fu); }
 };
 
