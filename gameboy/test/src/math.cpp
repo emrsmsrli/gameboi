de59cdd031b2bae9aef619e7e5b0717408a30b38
emrsmsrli <emrsmsrli1@gmail.com>
2020-01-02 00:44:23 +0300

    add math tests

---
+++ gameboy/test/src/math.cpp
@@ -0,0 +1,32 @@
+#include "gtest/gtest.h"
+#include "gameboy/util/mathutil.h"
+
+TEST(math, mask_test) {
+    ASSERT_TRUE(gameboy::mask_test(0x100, 0x100));
+    ASSERT_FALSE(gameboy::mask_test(0x100, 0x01));
+    ASSERT_TRUE(gameboy::mask_test(0xFC, 0x0C));
+    ASSERT_TRUE(gameboy::mask_test(0x111, 0x110));
+}
+
+TEST(math, mask_reset) {
+    ASSERT_EQ(gameboy::mask_reset(0x100, 0x100), 0x0);
+    ASSERT_EQ(gameboy::mask_reset(0x100, 0x01), 0x100);
+    ASSERT_EQ(gameboy::mask_reset(0xFC, 0x0C), 0xF0);
+    ASSERT_EQ(gameboy::mask_reset(0x111, 0x110), 0x1);
+}
+
+TEST(math, bit_test) {
+    ASSERT_FALSE(gameboy::bit_test(0x0, 7));
+    ASSERT_TRUE(gameboy::bit_test(0xFC, 15));
+    ASSERT_TRUE(gameboy::bit_test(0xFC, 14));
+    ASSERT_FALSE(gameboy::bit_test(0xFC, 0));
+    ASSERT_FALSE(gameboy::bit_test(0xFC, 1));
+}
+
+TEST(math, bit_reset) {
+    ASSERT_EQ(gameboy::bit_reset(0x01, 0), 0x0);
+    ASSERT_EQ(gameboy::bit_reset(0xFC, 15), 0x7C);
+    ASSERT_EQ(gameboy::bit_reset(0xFC, 14), 0xBC);
+    ASSERT_EQ(gameboy::bit_reset(0xFC, 0), 0xFC);
+    ASSERT_EQ(gameboy::bit_reset(0xFC, 1), 0xFC);
+}
