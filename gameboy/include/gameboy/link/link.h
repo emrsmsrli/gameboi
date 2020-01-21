caaad62d2d642984cd5d34171edf7e81ab590a6c
emrsmsrli <emrsmsrli1@gmail.com>
2020-01-22 00:58:45 +0300

    add link registers

--- gameboy/include/gameboy/link/link.h
+++ gameboy/include/gameboy/link/link.h
@@ -3,11 +3,30 @@
 
 #include <cstdint>
 
+#include "gameboy/util/observer.h"
+#include "gameboy/cpu/register8.h"
+
 namespace gameboy {
+
+class bus;
 
 class link {
 public:
+    explicit link(observer<bus> bus) noexcept;
+
     void tick(uint8_t cycles) noexcept;
+
+private:
+    observer<bus> bus_;
+
+    register8 sb_;
+    register8 sc_;
+    
+    void on_sb_write(const address16&, uint8_t data) noexcept;
+    [[nodiscard]] uint8_t on_sb_read(const address16&) const noexcept;
+
+    void on_sc_write(const address16&, uint8_t data) noexcept;
+    [[nodiscard]] uint8_t on_sc_read(const address16&) const noexcept;
 };
 
 } // namespace gameboy
--- gameboy/src/link/link.cpp
+++ gameboy/src/link/link.cpp
@@ -1,10 +1,50 @@
 #include "gameboy/link/link.h"
+#include "gameboy/bus.h"
+#include "gameboy/memory/address.h"
+#include "gameboy/memory/mmu.h"
 
 namespace gameboy {
+
+constexpr address16 sb_addr{0xFF01u};
+constexpr address16 sc_addr{0xFF02u};
+
+link::link(const observer<bus> bus) noexcept
+    : bus_{bus}
+{
+    auto mmu = bus_->get_mmu();
+    mmu->add_memory_delegate(sb_addr, {
+        {connect_arg<&link::on_sb_read>, this},
+        {connect_arg<&link::on_sb_write>, this}
+    });
+    mmu->add_memory_delegate(sc_addr, {
+        {connect_arg<&link::on_sc_read>, this},
+        {connect_arg<&link::on_sc_write>, this}
+    });
+}
 
 void link::tick(const uint8_t cycles) noexcept
 {
     
+}
+
+void link::on_sb_write(const address16&, uint8_t data) noexcept
+{
+    
+}
+
+uint8_t link::on_sb_read(const address16&) const noexcept
+{
+    return 0u;
+}
+
+void link::on_sc_write(const address16&, uint8_t data) noexcept
+{
+    
+}
+
+uint8_t link::on_sc_read(const address16&) const noexcept
+{
+    return 0u;
 }
 
 } // namespace gameboy
