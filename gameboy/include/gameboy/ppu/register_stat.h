9b4193d7f8d67860bcbdb7c28766d9f0f9af034d
emrsmsrli <emrsmsrli1@gmail.com>
2020-02-16 18:04:52 +0300

    cleanup stat

--- gameboy/include/gameboy/ppu/register_stat.h
+++ gameboy/include/gameboy/ppu/register_stat.h
@@ -15,28 +15,20 @@ enum class stat_mode : uint8_t {
 struct register_stat {
     register8 reg;
 
-    register_stat() = default;
     explicit register_stat(const uint8_t val)
         : reg{val} {}
 
-    [[nodiscard]] stat_mode get_mode() const noexcept
-    {
-        return static_cast<stat_mode>(reg.value() & 0x03u);
-    }
+    [[nodiscard]] stat_mode get_mode() const noexcept { return static_cast<stat_mode>(reg.value() & 0x03u); }
+    void set_mode(stat_mode mode) noexcept { reg = (reg & 0xFCu) | static_cast<uint8_t>(mode); }
 
-    void set_mode(stat_mode mode) noexcept
-    {
-        reg = (reg & 0xFCu) | static_cast<uint8_t>(mode);
-    }
+    [[nodiscard]] bool coincidence_flag_set() const noexcept { return bit_test(reg, 2u); }
+    void set_coincidence_flag() noexcept { reg = bit_set(reg, 2u); }
+    void reset_coincidence_flag() noexcept {  reg = bit_reset(reg, 2u); }
 
     [[nodiscard]] bool mode_interrupt_set() const noexcept
     {
         return bit_test(reg, 1u << static_cast<uint8_t>(get_mode()));
     }
-
-    [[nodiscard]] bool coincidence_flag_set() const noexcept { return bit_test(reg, 2u); }
-    void set_coincidence_flag() noexcept { reg = bit_set(reg, 2u); }
-    void reset_coincidence_flag() noexcept {  reg = bit_reset(reg, 2u); }
 };
 
 } // namespace gameboy
