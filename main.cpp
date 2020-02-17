bafe21127f3b14e008713e85955e0bc338c0ff89
emrsmsrli <emrsmsrli1@gmail.com>
2020-02-17 22:56:48 +0300

    implement resizable sfml window

--- main.cpp
+++ main.cpp
@@ -13,37 +13,41 @@ struct renderer {
     sf::Sprite window_sprite;
     sf::RenderWindow window;
 
-    float scale;
-    sf::Vector2f position;
-
     explicit renderer(gameboy::gameboy& gb, const uint32_t width, const uint32_t height)
         : window{
             sf::VideoMode(width, height),
             fmt::format("GAMEBOY - {}", gb.rom_name()),
             sf::Style::Default
-        },
-        scale{
-            width > height
-              ? height / static_cast<float>(gameboy::screen_height)
-              : width / static_cast<float>(gameboy::screen_width)
-        },
-        position{
-            (width - gameboy::screen_width * scale) * .5f,
-            (height - gameboy::screen_height * scale) * .5f
         }
     {
-        window.setFramerateLimit(500);
         window_buffer.create(gameboy::screen_width, gameboy::screen_height, sf::Color::White);
         window_texture.create(gameboy::screen_width, gameboy::screen_height);
-
+        
+        const auto sprite_local_bounds = window_sprite.getLocalBounds();
+        window_sprite.setOrigin(sprite_local_bounds.width * .5f, sprite_local_bounds.height * .5f);
         window_sprite.setTexture(window_texture);
-        window_sprite.setScale(scale, scale);
-        window_sprite.setPosition(position);
 
+        rescale(width, height);
         render_frame();
 
         gb.on_render_line({gameboy::connect_arg<&renderer::render_line>, this});
         gb.on_vblank({gameboy::connect_arg<&renderer::render_frame>, this});
+    }
+
+    void rescale(const uint32_t width, const uint32_t height)
+    {
+        const auto sprite_local_bounds = window_sprite.getLocalBounds();
+
+        const auto screen_aspect_ratio = static_cast<float>(width) / height;
+        const auto sprite_aspect_ratio = sprite_local_bounds.width / sprite_local_bounds.height;
+        const auto scale = screen_aspect_ratio > sprite_aspect_ratio
+            ? height / sprite_local_bounds.height
+            : width / sprite_local_bounds.width;
+        
+        window_sprite.setScale(scale, scale);
+        window_sprite.setPosition(width * .5f, height * .5f);
+
+        draw_sprite();
     }
 
     void render_line(const uint8_t line_number, const gameboy::render_line& line)
@@ -56,13 +60,17 @@ struct renderer {
         }
     }
 
-    void render_frame()
+    void draw_sprite()
     {
-        window_texture.update(window_buffer);
-
         window.clear();
         window.draw(window_sprite);
         window.display();
+    }
+
+    void render_frame()
+    {
+        window_texture.update(window_buffer);
+        draw_sprite();
     }
 };
 
@@ -98,6 +106,10 @@ int main(int argc, char* argv[])
         while(renderer.window.pollEvent(event)) {
             if(event.type == sf::Event::Closed) {
                 renderer.window.close();
+            } else if(event.type == sf::Event::Resized) {
+                const sf::FloatRect visible_area(0, 0, event.size.width, event.size.height);
+                renderer.window.setView(sf::View{visible_area});
+                renderer.rescale(event.size.width, event.size.height);
             } else if(event.type == sf::Event::KeyPressed) {
                 switch(event.key.code) {
                     case sf::Keyboard::Up:
