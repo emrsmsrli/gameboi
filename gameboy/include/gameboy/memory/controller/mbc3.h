b48f2a0be04c01194a62ba12067991b10376cd03
emrsmsrli <emrsmsrli1@gmail.com>
2020-02-23 14:26:13 +0300

    remove default constructors of mbc3 and rtc

--- gameboy/include/gameboy/memory/controller/mbc3.h
+++ gameboy/include/gameboy/memory/controller/mbc3.h
@@ -13,8 +13,6 @@ class rtc {
 public:
     bool enabled = false;
 
-    rtc() = default;
-
     [[nodiscard]] uint8_t read() const noexcept;
     void write(uint8_t data) noexcept;
     void latch() noexcept;
@@ -34,8 +32,6 @@ private:
 
 class mbc3 : public mbc {
 public:
-    mbc3() = default;
-
     void control(const address16& address, uint8_t data) noexcept;
 
     [[nodiscard]] uint8_t read_ram(const std::vector<uint8_t>& ram, const physical_address& address) const;
