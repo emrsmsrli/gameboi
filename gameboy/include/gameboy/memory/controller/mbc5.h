b3ba1be74596ffbea3b390d91e97612ebc156073
emrsmsrli <emrsmsrli1@gmail.com>
2020-02-23 14:45:25 +0300

    remove mbc5::rom_banking_active

--- gameboy/include/gameboy/memory/controller/mbc5.h
+++ gameboy/include/gameboy/memory/controller/mbc5.h
@@ -10,8 +10,6 @@
 namespace gameboy {
 
 struct mbc5 : mbc {
-    bool rom_banking_active = true;
-
     void control(const address16& address, uint8_t data) noexcept;
 
     [[nodiscard]] uint8_t read_ram(const std::vector<uint8_t>& ram, const physical_address& address) const;
--- gameboy/src/memory/controller/mbc5.cpp
+++ gameboy/src/memory/controller/mbc5.cpp
@@ -2,7 +2,6 @@
 
 #include "gameboy/memory/controller/mbc5.h"
 #include "gameboy/memory/address_range.h"
-#include "gameboy/util/mathutil.h"
 
 namespace gameboy {
 
@@ -12,7 +11,6 @@ void mbc5::control(const address16& address, const uint8_t data) noexcept
     constexpr address_range rom_bank_select_low_range{0x2000u, 0x2FFFu};
     constexpr address_range rom_bank_select_high_range{0x3000u, 0x3FFFu};
     constexpr address_range ram_bank_select_range{0x4000u, 0x5FFFu};
-    constexpr address_range memory_mode_select_range{0x6000u, 0x7FFFu};
 
     if(external_ram_enable_range.has(address)) {
         set_xram_enabled(data);
@@ -22,8 +20,6 @@ void mbc5::control(const address16& address, const uint8_t data) noexcept
         rom_bank = (rom_bank & ~0x0010u) | ((data & 0x1u) << 8u);
     } else if(ram_bank_select_range.has(address)) {
         ram_bank = data & 0x0Fu;
-    } else if(memory_mode_select_range.has(address)) {
-        rom_banking_active = !bit_test(data, 0x1u);
     }
 }
 
