cb28b40c06e48b5744af6e79b263da648b43f953
Emre <emrsmsrli1@gmail.com>
2020-03-18 21:36:21 +0300

    use lcov for travis coverage report

---
+++ scripts/report_coverage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "${COV}" = "1" ]; then
+    lcov --directory . --capture --output-file coverage.info
+    lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' --output-file coverage.info
+    lcov --list coverage.info
+    bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
+fi
