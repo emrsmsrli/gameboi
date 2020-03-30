6abcf03c0f5ad3497c16dd5d59881788262db8ed
emrsmsrli <emrsmsrli1@gmail.com>
2020-03-30 14:26:19 +0300

    update version
    - fix obj priority issues
    - fix obj wrong palette usage
    - fix mbc banking issues
    - fix 32 kb rom has the incorrect number of banks
    - implement unused bits (most of them anyway) now return 1 instead of 0
    - implement only one internal clock for timer instead of two

--- gameboy/include/gameboy/version.h
+++ gameboy/include/gameboy/version.h
@@ -7,9 +7,9 @@ namespace gameboy::version {
 
 [[maybe_unused]] constexpr auto major = 0;
 [[maybe_unused]] constexpr auto minor = 6;
-[[maybe_unused]] constexpr auto patch = 0;
+[[maybe_unused]] constexpr auto patch = 1;
 
-[[maybe_unused]] constexpr std::string_view version = "0.6.0";
+[[maybe_unused]] constexpr std::string_view version = "0.6.1";
 
 } // namespace gameboy::version
 
