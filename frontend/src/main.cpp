2dd79b194eb059c687a0bbb0fa2a2db3183a8d2c
emrsmsrli <emrsmsrli1@gmail.com>
2020-05-25 19:27:34 +0300

    fix busy wait while out of focus missing a continue

--- frontend/src/main.cpp
+++ frontend/src/main.cpp
@@ -145,6 +145,7 @@ int main(int argc, char* argv[])
         ) {
             using namespace std::chrono_literals;
             std::this_thread::sleep_for(50ms);
+            continue;
         }
 
         gb.tick_one_frame();
